cf787d0112396915ac1330ddc6bbf086d6b88050
yosiefted <yosief.ted@gmail.com>
2017-05-17 21:16:21 +0000

    added s3 to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Added as per Devise installation Instruction
+  config.action_mailer.default_url_options = { host: 'pinterested-yt.herokuapp.com'}
+  
+  # config/environments/production.rb
+  config.paperclip_defaults = {
+  :storage: :s3,
+  :s3_credentials: {
+    :bucket: ENV.fetch('S3_BUCKET_NAME'),
+    :access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    :secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+  }
+}
 end
